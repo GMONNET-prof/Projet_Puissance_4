9155788bc63cab325dd1df0766a6dc9c02a9f847
Joan <jojomatt88@gmail.com>
2026-06-05 10:49:22 +0200

    Create verification_verticale.py

---
+++ verification_verticale.py
@@ -0,0 +1,23 @@
+def verif_vertical(grille, joueur):
+
+    i = NB_PIONS_ALIGNES - 1
+
+    while i < NB_LIGNES:
+
+        j = 0
+
+        while j < NB_COLONNES:
+
+            z = 0
+
+            while z < NB_PIONS_ALIGNES and grille[i - z][j] == joueur:
+                z += 1
+
+            if z == NB_PIONS_ALIGNES:
+                return True
+
+            j += 1
+
+        i += 1
+
+    return False
